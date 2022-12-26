1ea6645eb3a1294a98b3988d445640529e599f03
Ian Iversen <ianiversen1@gmail.com>
2022-12-25 19:00:46 -0500

    inputs being controlled, now setting them to state

--- age-calculator/src/Components/AgeCalculator.js
+++ age-calculator/src/Components/AgeCalculator.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AgeCalculator() {
+
+    const [month, setMonth] = useState(0);
+    const [day, setDay] = useState(0);
+    const [year, setYear] = useState(0);
+
+    const handleMonth = (e) => {
+        console.log(e.target.value)
+    }
+    const handleDay = (e) => {
+        console.log(e.target.value)
+    }
+    const handleYear = (e) => {
+        console.log(e.target.value)
+    }
+
   return (
     <div className='AgeCalculator'>
         <h1>Age <br/> Calculator</h1>
         <h3>How old are you?</h3>
         <form>
-            <label for='month'>Month Born:</label>
-            <input id='month' placeholder='mm'></input>
+            <label htmlFor='month'>Month Born:</label>
+            <input id='month' placeholder='mm' onChange={handleMonth}></input>
             
-            <label for='day'>Day Born:</label>
-            <input id='day' placeholder='dd'></input>
+            <label htmlFor='day'>Day Born:</label>
+            <input id='day' placeholder='dd' onChange={handleDay}></input>
 
-            <label for='year'>Year Born:</label>
-            <input id='year' placeholder='yyyy'></input>
+            <label htmlFor='year'>Year Born:</label>
+            <input id='year' placeholder='yyyy' onChange={handleYear}></input>
             
             <button type='submit'>Submit</button>
         </form>
