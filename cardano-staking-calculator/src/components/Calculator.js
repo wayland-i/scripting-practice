41e27351d137feac1f7c86dc4fbde23311005e03
Ian Iversen <ianiversen1@gmail.com>
2023-01-04 22:31:30 -0500

    mvp reached

--- cardano-staking-calculator/src/components/Calculator.js
+++ cardano-staking-calculator/src/components/Calculator.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Calculator() {
+  const [ada, setAda] = useState("")
+  const [rewards, setRewards] = useState("")
+
+  const stakingRewards = (ada) => {
+    return setRewards(ada * .0461);
+  }
+
+  const handleInput = (e) => {
+    setAda(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    stakingRewards(ada);
+    setAda("");
+  }
+
   return (
     <div>
         <h1>calculator</h1>
+        <form>
+          <h2>How much ADA will you stake for the year?</h2>
+          <input type='text' placeholder='ex. 1,000' onChange={handleInput} value={ada}/>
+          <button type='submit' onClick={handleSubmit}>submit</button>
+        </form>
+        {rewards ? <h1>{rewards}</h1> : null}
     </div>
   )
 }
