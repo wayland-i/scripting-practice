ee2808ff717ec7a2b8814bdd511d962645304879
Ian Iversen <ianiversen1@gmail.com>
2023-01-10 22:37:38 -0500

    calculator completed

--- simple-calculator-functions/src/App.jsx
+++ simple-calculator-functions/src/App.jsx
@@ -10,39 +10,64 @@ const Nums = styled.input`
     outline: none;
     background-color: transparent;
     margin: 1%;
+    max-width: 8rem;
+    font-size: 5rem;
+    text-align: center;
 `
 
 function App() {
-  const [numOne, setNumOne] = useState(null);
-  const [numTwo, setNumTwo] = useState(null);
+  const [numOne, setNumOne] = useState("");
+  const [numTwo, setNumTwo] = useState("");
+  const [result, setResult] = useState("");
 
-
+  const handleOnChange = (e) => {
+    if (e.target.id == 1){
+      setNumOne(e.target.value);
+    } else if (e.target.id == 2){
+      setNumTwo(e.target.value);
+    }
+  }
   const handleMath = (e) => {
     const operator = e.target.name;
     // console.log(operator);
     
     if (operator === "add") {
-      console.log(numOne + numTwo)
+      setResult(parseInt(numOne) + parseInt(numTwo));
     } else if (operator === "subtract") {
-      console.log("sub clicked");
+      setResult(parseInt(numOne) - parseInt(numTwo));
     } else if (operator === "divide") {
-      console.log("div clicked");
+      setResult(parseInt(numOne) / parseInt(numTwo));
     } else if (operator === "multiply") {
-      console.log("mult clicked");
+      setResult(parseInt(numOne) * parseInt(numTwo));
     }
+
+    setTimeout(() => {
+      const InputOne = document.getElementById('1');
+      const InputTwo = document.getElementById('2');
+      InputOne.value = "";
+      InputTwo.value = "";
+    }, 3000)
   
   }
 
   return (
     <div className="App">
-      <span style={{ "display": "block", "margin": "5%"}}>
-        <Nums></Nums>
-        <Nums></Nums>
-      </span>
+      <h1 style={{ "margin-bottom": "10%"}}>num 1 (+, -, /, *) num 2</h1>
+        <span style={{ "display": "block", "margin": "5%"}}>
+          <Nums id='1' onChange={handleOnChange}></Nums>
+          <Nums id='2' onChange={handleOnChange}></Nums>
+          <span style={{ "display": "block" }}>
+            <label htmlFor='1' style={{ "margin-right": "8%"}}>num 1</label>
+            <label htmlFor='2' style={{ "margin-left": "5%"}}>num 2</label>
+          </span>
+        </span>
       <Button handleMath={handleMath} name="add"/>
       <Button handleMath={handleMath} name="subtract"/>
       <Button handleMath={handleMath} name="divide"/>
       <Button handleMath={handleMath} name="multiply"/>
+
+      {result ? <h1>Result: {result}</h1> : <h1>Use my app!</h1>}
+
     </div>
   )
 }
